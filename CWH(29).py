725e1ad902d2ea613d40176c5e905c07992a3521
Karan Nair <elitekarry@gmail.com>
2026-01-01 19:08:03 +0530

    DOC Strings

---
+++ CWH(29).py
@@ -0,0 +1,11 @@
+# DOC Strings
+def square(n):
+    '''Takes in a number n, returns the square of n'''
+    print(n**2)
+square(5)
+print(square.__doc__)
+
+# used when multiple programmers work on large project together
+
+import this
+# it is the poem my tim peters (easter egg)
