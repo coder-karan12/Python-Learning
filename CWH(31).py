ba711e6b8b4504ab3fc1275cc730d4fe9b82eae4
Karan Nair <elitekarry@gmail.com>
2026-01-06 13:30:25 +0530

    Add files via upload

---
+++ CWH(31).py
@@ -0,0 +1,17 @@
+# Sets In Python
+
+# Sets are unorderes collection of data items.
+
+s = {8,4,2,6,4}
+print(s)
+
+info = {"carla",19,False,5.9,19}
+print(info)
+
+# Empty Set
+harry = set()
+print(type(harry))
+
+# Using for loop
+for value in info:
+    print(value)
