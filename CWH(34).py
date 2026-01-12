8536c885845e90cf7fe901ec8993c3bfc4292223
Karan Nair <elitekarry@gmail.com>
2026-01-12 21:56:50 +0530

    Add files via upload

---
+++ CWH(34).py
@@ -0,0 +1,25 @@
+# Dictionary Methods
+# --> update()
+ep1 = {122: 45,186:89, 212:64, 670: 56}
+ep2 = {222: 67, 312: 96}
+
+# ep1.update(ep2)
+
+# --> clear()
+# ep1.clear()
+
+# --> pop()
+# ep1.pop(122)
+
+# --> popitem()
+# removes last item of dict
+# ep1.popitem()
+
+# --> del()
+# deletes dict
+# del ep1
+del ep1[122] #deletes specific number
+print(ep1)
+
+
+# for more method go in python dict documentation
