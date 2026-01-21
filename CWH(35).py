84423e108f89d8689cebfa743701ca200d2fa58f
Karan Nair <elitekarry@gmail.com>
2026-01-21 10:55:36 +0530

    Add files via upload

---
+++ CWH(35).py
@@ -0,0 +1,18 @@
+# for loop with else:
+for i in range(6):
+    print(i)
+    if i == 4:
+        break
+
+else:
+    print("Sorry no i ")
+
+# ---> else can be also used with while loop
+
+i = 0
+while i<7:
+    print(i)
+    i = i + 1
+
+else:
+    print("Sorry no i ")
